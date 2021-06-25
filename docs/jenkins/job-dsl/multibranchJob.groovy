a33a121e84f47c4eaa7ac5091e0a361f195e86eb
Muhammad Sidiq Putra <sidiq.putra@tiket.com>
2021-06-25 10:02:08 +0700

    create multibranch job dsl

---
+++ docs/jenkins/job-dsl/multibranchJob.groovy
@@ -0,0 +1,44 @@
+import groovy.json.JsonSlurper
+
+def inputFile = readFileFromWorkspace("docs/jenkins/job-dsl/pipelineJob.json")
+def InputJSON = new JsonSlurper().parseText(inputFile)
+
+
+for (i = 0; i < InputJSON.project.size(); i++) {
+def project_name = InputJSON.project[i].name
+
+    multibranchPipelineJob("PR-${project_name}") {
+        branchSources {
+            github {
+                id("${project_name}")
+                scanCredentialsId('github-token')
+                repository('https://github.com/msidiqputra/' + project_name + '')
+                buildOriginBranch(false)
+                buildOriginBranchWithPR(true)
+                buildForkPRMerge(false)
+                buildOriginPRMerge(true)
+            }
+        }
+
+        configure {
+            def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+
+            traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+                strategyId('1')
+            }
+            traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+                strategyId('1')
+            }
+
+            traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+                includes('PR-*')
+                excludes('')
+            }
+            def factory = it / factory(class: 'com.cloudbees.workflow.multibranch.CustomBranchProjectFactory')
+            factory << definition(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition') {
+                scriptPath('Jenkinsfile')
+            }
+
+        }
+    }
+}
